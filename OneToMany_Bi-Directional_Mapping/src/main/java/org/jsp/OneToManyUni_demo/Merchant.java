ab93804e9b390caa9ca081332426590fd415a386
Shashi <ranjanshashi4862@gmail.com>
2025-02-15 10:24:45 +0530

    OneToMany Bi-Directional mapping

---
+++ OneToMany_Bi-Directional_Mapping/src/main/java/org/jsp/OneToManyUni_demo/Merchant.java
@@ -0,0 +1,69 @@
+package org.jsp.OneToManyUni_demo;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+public class Merchant {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private long phone;
+	private String gst_number;
+	private String email;
+	private String password;
+	
+	@OneToMany(mappedBy = "merchant", cascade = CascadeType.ALL)
+	private List<Product> products;
+	
+	public List<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getGst_number() {
+		return gst_number;
+	}
+	public void setGst_number(String gst_number) {
+		this.gst_number = gst_number;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Merchant [id=" + id + ", name=" + name + ", phone=" + phone + ", gst_number=" + gst_number + ", email="
+				+ email + ", password=" + password + "]";
+	}
+	
+}
---
+++ OneToMany_Bi-Directional_Mapping/src/main/java/org/jsp/OneToManyUni_demo/Product.java
@@ -0,0 +1,53 @@
+package org.jsp.OneToManyUni_demo;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String brand;
+	private String category;
+	
+	@JoinColumn
+	@ManyToOne
+	private Merchant merchant;
+	
+	public Merchant getMerchant() {
+		return merchant;
+	}
+	public void setMerchant(Merchant merchant) {
+		this.merchant = merchant;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", brand=" + brand + ", category=" + category + "]";
+	}
+	
+}
